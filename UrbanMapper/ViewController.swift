c89898b0de65e21607b35633f752150fcb0d3697
PrajnyaPrabhu19 <prajnya.prabhu@email.ucr.edu>
2020-11-26 19:16:09 -0800

    Initial Commit

---
+++ UrbanMapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrbanMapper
+//
+//  Created by Prajnya Prabhu on 11/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
